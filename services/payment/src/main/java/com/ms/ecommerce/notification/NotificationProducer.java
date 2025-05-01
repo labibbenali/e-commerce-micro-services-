12825abaa91de84a1a476d1df80f2790ee756537
Labib BENALI <labib9774@gmail.com>
2025-05-01 18:17:02 +0200

    set up notification class

---
+++ services/payment/src/main/java/com/ms/ecommerce/notification/NotificationProducer.java
@@ -0,0 +1,30 @@
+package com.ms.ecommerce.notification;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+import static org.springframework.kafka.support.KafkaHeaders.*;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class NotificationProducer {
+
+    private final KafkaTemplate<String, PaymentNotificationRequest> kafkaTemplate;
+
+    public void send(final PaymentNotificationRequest request) {
+        log.info("Sending  notification with boduy <{}>", request);
+        Message<PaymentNotificationRequest> message = MessageBuilder
+                .withPayload(request)
+                .setHeader(TOPIC, "payment-topic")
+                .build();
+        kafkaTemplate.send(message);
+    }
+
+
+
+}
